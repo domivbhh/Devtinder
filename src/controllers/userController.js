f6034bab21838506085e6c26bfee49de65f6d67b
domivbhh <hariharanvbit123@gmail.com>
2024-09-17 16:17:47 +0530

    signup validation,update user validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,19 +1,24 @@
 const ErrorHandler = require("../middleware/errorHandler")
 const dataSend = require("../middleware/sendData")
 const User = require("../models/user")
+const validator = require("validator");
+
 
 const signUp=async(req,res,next)=>{
     try {
         const{firstName,lastName,gender,password,emailId,age}=req.body
-
-            if(!firstName || !lastName || !emailId || !password ||!age ||!gender){
+         if(!firstName || !lastName || !emailId || !password ||!age ||!gender){
                 return next(new ErrorHandler(400,'Fill the require Details'))
-            }
+        }
+        if(!validator.isEmail(emailId)){
+                return next(new ErrorHandler(400, "Invalid Email_id"));
+        }
 
         const newUser=new User({firstName,lastName,emailId,gender,age,password})
         await newUser.save()
         if(newUser._id){
-            dataSend(res=res,message='user created succesfully',data=newUser)
+            const sendingData=await User.findById(newUser._id).select('-password')
+            dataSend(res=res,message='user created succesfully',data=sendingData)
         }        
     } 
     catch (error) {
@@ -22,5 +27,39 @@ const signUp=async(req,res,next)=>{
 }
 
 
+const getAllUser=async(req,res,next)=>{
+    try {
+            
+    } 
+    catch (error) {
+        
+    }
+}
 
-module.exports={signUp}
+const updateUser=async(req,res,next)=>{
+    try {
+        const datas=req.body
+        const {id}=req.params
+        const allowed=['firstName','lastName','age','gender']
+        const isUpdateAllowed=Object.keys(datas).some((ele)=>allowed.includes(ele))
+        if (isUpdateAllowed) {
+          const user = await User.findByIdAndUpdate(id, datas, {
+            new: true,
+            runValidators: true,
+          });
+          if (user._id) {
+            dataSend(res, (message = "user updated successfully"), user);
+          }
+        } else {
+          return next(new ErrorHandler(401, `Invalid Field Update`));
+        }
+        
+    } 
+    catch (error) {
+        next(new ErrorHandler(401, error.message));
+    }
+}
+
+
+
+module.exports={signUp,updateUser}
--- src/models/user.js
+++ src/models/user.js
@@ -1,37 +1,86 @@
 const mongoose=require('mongoose')
+const bcrypt=require('bcryptjs')
+const validator=require('validator')
 
-const userSchema=new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
-        minlength:3
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
     },
-    lastName:{
-        type:String,
-        required:true
+    lastName: {
+      type: String,
+      required: true,
     },
-    emailId:{
-        type:String,
-        unique:true,
-        required:true
+    emailId: {
+      type: String,
+      unique: true,
+      required: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address");
+        }
+      },
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
+      select: false,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Weak password");
+        }
+      },
     },
-    age:{
-        type:Number,
-        required:true
+    age: {
+      type: Number,
+      required: true,
+      min: [15, "Minimum age must be 15"],
     },
-    gender:{
-        type:String,
-        enum:['male','female','others'],
-        required:true
-    }
-},{
-    timestamps:true
+    gender: {
+      type: String,
+      enum: ["male", "female", "others"],
+      required: true,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Invalid gender");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "Fresher in the community",
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://cdn.vectorstock.com/i/500p/53/42/user-member-avatar-face-profile-icon-vector-22965342.jpg",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid photo url");
+        }
+      },
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save',async function(next){
+    if(this.isModified("password"))
+        {
+               this.password=await bcrypt.hash(this.password,10)
+                next()
+        }
 })
 
-const User=mongoose.model('users',userSchema)
 
+
+const User=mongoose.model('users',userSchema)
 module.exports=User
